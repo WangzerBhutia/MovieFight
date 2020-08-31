ee9b8db53efa09f01367d15296f4e38de0dbcb30
Wangzer Bhutia <01wangzer@gmail.com>
2020-08-31 19:49:00 +0530

    Reusable autocomplete widget

--- index.js
+++ index.js
@@ -1,50 +1,87 @@
-const fetchData = async (searchTerm) => {
-    const response = await axios.get("http://www.omdbapi.com/", {
+
+// Autocomplete configuration object
+createAutocomplete({ 
+    // root: where to render the autocomplete widget
+    root: document.querySelector('.autocomplete'),
+    // how to show the content of the individual items
+    renderOption(movie){
+        const imgSrc = movie.Poster === 'N/A' ? '': movie.Poster;
+        return `
+                <img src="${imgSrc}" />
+                ${movie.Title} (${movie.Year})
+            `;
+    },
+    // What to do when an item is pressed
+    onOptionSelect(movie){
+        onMovieSelect(movie);
+    },
+    // What to backfill into the input when an item is selected
+    inputValue(movie){
+        return movie.Title;
+    },
+    // How to fetch the data
+    async fetchData (searchTerm){
+        const response = await axios.get("http://www.omdbapi.com/", {
+            params: {
+                apikey: '2e832554',
+                s: searchTerm,
+            }
+        });
+        if(response.data.Error) {
+            return [];
+        }
+        return response.data.Search;
+    }
+})
+
+
+const onMovieSelect = async (movie) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '2e832554',
-            s: searchTerm,
+            i: movie.imdbID,
         }
     });
-    if(response.data.Error) {
-        return [];
-    }
-    return response.data.Search;
-};
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+}
 
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-    <label><b>Search for a Movie</b></label>
-    <br>
-    <input class="input"/>
-    <div class="dropdown">
-        
-        <div class="dropdown-menu">
-            <div class="dropdown-content results"></div>
-        </div>
-    </div>`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async event => {
-    const movies = await fetchData(event.target.value);
-
-    dropdown.classList.add('is-active');
-    for(let movie of movies) {
-        const option = document.createElement('a');
-
-        option.classList.add('dropdown-item');
-        option.innerHTML = `
-            <img src="${movie.Poster}" />
-            ${movie.Title}
+const movieTemplate = (movieDetail) => {
+    return `
+        <article class="media">
+            <figure class="media-left">
+                <p class="image">
+                    <img src="${movieDetail.Poster}"/>
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+                </div>
+            </div>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Awards}</p>
+            <p class="subtitle">Awards</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.BoxOffice}</p>
+            <p class="subtitle">Box Office</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Metascore}</p>
+            <p class="subtitle">Metascore</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbRating}</p>
+            <p class="subtitle">IMDB Rating</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbVotes}</p>
+            <p class="subtitle">IMDB Votes</p>
+        </article>
         `;
-        console.log(movie);
+}
 
-        resultsWrapper.appendChild(option);
-    }
-};
-
-
-input.addEventListener('input', debounce(onInput, 1000));
